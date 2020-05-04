397856ca6253285c65c852cf5ef81cd66dcce09f
Kathryn Pritchard <pritchard_kathryn@bah.com>
2020-05-03 20:21:22 -0400

    p3 - add animation to nav hamburger menu

--- p3/submission/scripts/main.js
+++ p3/submission/scripts/main.js
@@ -1,5 +1,5 @@
 	$(document).ready(function () {		
-		$('#bars').on('click', toggleNav);
+		$('.nav-icon').on('click', toggleNav);
 		$('#cancel').on('click', toggleNav);
 	});
 	
@@ -9,10 +9,19 @@
 				$('#links').hide();
 				$('#cancel').hide();
 				$('#bars').show();
+				$('.hamburger-line-1').css('transform', 'translateY(0px) rotate(0deg)');
+				$('.hamburger-line-2').css('transform', 'scale(1)');
+				$('.hamburger-line-3').css('transform', 'translateY(0px) rotate(0deg)');
 			}
 			else {
 				$('#links').show();			
 				$('#cancel').show();
 				$('#bars').hide();
+				$('.hamburger-line-1').css('transform', 'translateY(8px) rotate(135deg)');
+				$('.hamburger-line-2').css('transform', 'scale(0)');
+				$('.hamburger-line-3').css('transform', 'translateY(-8px) rotate(-135deg)');
+				
 			}
 	}
+	
+	
